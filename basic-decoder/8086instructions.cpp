63f6ed3b3574d5137e9b4714179a218bf330c0a0
MBDur12 <rgfb35@durham.ac.uk>
2023-12-31 16:36:52 +0000

    wip

--- basic-decoder/8086instructions.cpp
+++ basic-decoder/8086instructions.cpp
@@ -26,15 +26,16 @@ vector<instruction> BuildInstructions(u8 *Source, size_t FileSize)
     while (CurrentIdx < FileSize)
     {
         instruction NewInstruction;
+
         u8 Byte1 = Source[CurrentIdx];
         u8 Op = (Byte1 & 0b11111100) >> 2;
 
         if (Op == 0b101100) // imm-to-reg
         {
-            ++CurrentIdx;
             u8 W = (Byte1 & 0b00001000) >> 3;
-            s16 Data = Source[CurrentIdx];
-
+            s16 Data = Source[++CurrentIdx];
+            
+            
             if (W == 1)
             {
                 Data = (Source[++CurrentIdx] << 8) | Data; // 2nd byte is most significant
@@ -47,29 +48,25 @@ vector<instruction> BuildInstructions(u8 *Source, size_t FileSize)
             
             NewInstruction.Operands[1].Type = operand_type::Operand_Imm;
             NewInstruction.Operands[1].Immediate = {Data};
-            ++CurrentIdx;
         }
         else if (Op == 0b100010) // reg/mem to/from reg
         {
-            ++CurrentIdx;
-            u8 Byte2 = Source[CurrentIdx];
+            NewInstruction.Op = op_type::Op_mov;
+            u8 Byte2 = Source[++CurrentIdx];
             u8 Mod = (Byte2 & 0b11000000) >> 6;
-
+            u8 W = Byte1 & 0b00000001;
+            u8 D = (Byte1 & 0b00000010) >> 1;
+            u8 Reg = (Byte2 & 0b00111000) >> 3;
+            u8 Rm = Byte2 & 0b00000111;
+            
             if (Mod == 0b11) // reg-to-reg
             {
-                u8 W = Byte1 & 0b00000001;
-                u8 D = (Byte1 & 0b00000010) >> 1;
-                
-                u8 Reg = (Byte2 & 0b00111000) >> 3;
-                u8 Rm = Byte2 & 0b00000111;
-
-                NewInstruction.Op = op_type::Op_mov;
                 NewInstruction.Operands[0].Type = operand_type::Operand_Reg;
                 NewInstruction.Operands[1].Type = operand_type::Operand_Reg;
                 NewInstruction.Operands[0].Register.Offset = W;
                 NewInstruction.Operands[1].Register.Offset = W;    
 
-                if (D == 0) // Reg is src
+                if (D == 0) // REG is src
                 {
                     NewInstruction.Operands[0].Register.Index = Reg;
                     NewInstruction.Operands[1].Register.Index = Rm; 
@@ -79,17 +76,46 @@ vector<instruction> BuildInstructions(u8 *Source, size_t FileSize)
                     NewInstruction.Operands[0].Register.Index = Rm;
                     NewInstruction.Operands[1].Register.Index = Reg; 
                 }
-                ++CurrentIdx;
+                
             }
-            else
+            else // Memory involved
             {
+                bool DirectlyAddressable = Mod == 0b00 && Rm == 0b110;
+                
+                // Compute displacement (if any)
+                s16 Displacement{};
+
+                if (Mod == 0b01) // 8-bit
+                {
+                    Displacement = Source[++CurrentIdx];
+                }
+                else if (Mod == 0b10 || DirectlyAddressable) // 16-bit
+                {
+                    Displacement = (Source[CurrentIdx + 2] << 8) | Source[CurrentIdx + 1];
+                    CurrentIdx += 2;
+                }   
+
+
+                if (D == 0) // REG is src
+                {
+                    NewInstruction.Operands[0].Type = operand_type::Operand_Reg;
+                    NewInstruction.Operands[0].Register.Index = Reg;
+                    NewInstruction.Operands[0].Register.Offset = W;
+                }
+                else
+                {
+                    NewInstruction.Operands[1].Type = operand_type::Operand_Reg;
+                    NewInstruction.Operands[1].Register.Index = Reg;
+                    NewInstruction.Operands[1].Register.Offset = W;
+                }
                 
             }
 
         }
 
         Instructions.push_back(NewInstruction);
-    }
+        ++CurrentIdx;
 
+    }
     return Instructions;
 }
